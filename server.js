a1edf084c84113d027cd46010c57d278990ee187
paulvvo <paulvo@live.ca>
2018-09-17 22:29:01 -0400

    added process environment port

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.put("/image", (req,res) =>{
 app.post("/imageurl", (req,res)=>{
   image.handleApiRequest(req,res);
 })
-app.listen(3001, ()=>{
+app.listen(process.env.PORT || 3001, ()=>{
   console.log("listening");
 });
